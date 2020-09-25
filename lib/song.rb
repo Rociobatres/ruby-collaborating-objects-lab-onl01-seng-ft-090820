54f0c482081abc1c928093b481017ae19297526d
Rocio Batres <jonasgirl21@gmail.com>
2020-09-25 22:30:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,11 +14,11 @@ class Song
  end 
  
  def self.new_by_filename(filename)
-   split_name = filename.split("-")  
-   song = split_name[0].strip
-   artist = split_name[1].strip
-   new_song = self.new(song)
-   
+  split_name = filename.split("-")  
+  song = split_name[0].strip
+  artist = split_name[1].strip
+  new_song = self.new(song)
+  new_song.artist_name = artist  
  end 
  
  def artist_name=(name)
