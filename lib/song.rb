aa3a0a7b7b664aa18e0f47ef162babc92c03aabb
Rocio Batres <jonasgirl21@gmail.com>
2020-09-25 22:21:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song 
+  
+ attr_accessor :name, :artist 
+ 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name 
+   @@all << self 
+ end 
+ 
+ def self.all
+   @@all 
+ end 
+ 
+ def self.new_by_filename(filename)
+   split_name = filename.split("-")  
+   song = self.new(split_name[0].strip)
+   artist = Artist.find_or_create_by_name(split_name[1].strip)
+   song.artist = artist
+ end 
+ 
+ def artist_name=(filename)
+    split_name = filename.split("-")
+    artist = Artist.find_or_create_by_name(split_name[1])
+    song.artist = artist
+    Artist.add_song(song)
+    song 
+ end 
+  
+end 
